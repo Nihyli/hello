141f368edb95880fa4e6587e909de18b6c935ba5
Yohannes <yhailu006@gmail.com>
2024-08-29 12:09:19 -0500

    code for name and major added

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Yohannes Hailu
+ * Date: 2024/08/29
+ * CSCE 155H
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Yohannes Hailu!\n");
+  printf("Computer Science")
+
+
+  return 0;
+}
